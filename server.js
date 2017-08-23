2ad80fa39f07d9019df2f8bd879b740144c88caa
Madhusuthanan <madhub081011@gmail.com>
2017-08-23 11:34:10 +0530

    Removing duplicate listen

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
-
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
