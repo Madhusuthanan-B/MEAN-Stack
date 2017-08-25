52a70ae5a54c1a6660552d742851b7eca1225c86
Madhusuthanan <madhub081011@gmail.com>
2017-08-25 21:25:13 +0530

    Using arrow functions

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const localDatabaseUrl = 'mongodb://localhost:27017/UsersDB';
 const url = process.env.MONGOLAB_URI || localDatabaseUrl;
+var port = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -22,31 +23,26 @@ app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-var port = process.env.PORT || 3000;
-
-app.get('/info', function (req, res) {
-    MongoClient.connect(url, function (err, db) {
+app.get('/info', (req, res) => {
+    MongoClient.connect(url, (err, db) => {
         if (err) {
             console.log('Connection failed with error:', err);
             res.send('Connection failed');
         } else {
-            console.log('Connection successful');
-            getAdminData(db, function (result) {
+            getAdminData(db, (result) => {
                 res.send(result[0]);
                 db.close();
             });
         }
     });
-
 });
 
-app.get('/mean', function (req, res) {
+app.get('/mean', (req, res) => {
     MongoClient.connect(url, function (err, db) {
         if (err) {
             console.log('Connection failed with error:', err);
             res.send('Connection failed');
         } else {
-            console.log('Connection successful');
             getHomeData(db, function (result) {
                 res.send(result[0]);
                 db.close();
@@ -56,15 +52,15 @@ app.get('/mean', function (req, res) {
 
 });
 
-var getAdminData = function (db, callback) {
+var getAdminData = (db, callback) => {
     var collection = (url === localDatabaseUrl) ? db.collection('Admin') : db.collection('about_admin');
-    collection.find({}).toArray(function (err, docs) {
+    collection.find({}).toArray((err, docs) => {
         callback(docs);
     });
 }
 
-var getHomeData = function (db, callback) {
-    var collection =db.collection('Home');
+var getHomeData = (db, callback) => {
+    var collection = db.collection('Home');
     collection.find({}).toArray(function (err, docs) {
         callback(docs);
     });
