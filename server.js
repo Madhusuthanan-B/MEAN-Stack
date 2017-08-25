5b69f8d7d8215e41f65cd9d65ab6327e66047e42
Madhusuthanan <madhub081011@gmail.com>
2017-08-25 22:02:58 +0530

    changing the path reference

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('./server/index');
+const app = require('./server');
 const port = process.env.PORT || 3000
 app.listen(port);
 console.log('Node server running in port:', port);
